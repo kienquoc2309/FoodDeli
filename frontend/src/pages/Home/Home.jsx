8d681dc978a1cf1d94cf73f7bf68cc0770ab448a
Tran Kien Quoc <kienquocthk9@gmail.com>
2024-11-28 19:03:25 +0700

    Complete UI

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import { useState } from 'react';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
 	const [category, setCategory] = useState('All');
@@ -13,6 +14,7 @@ const Home = () => {
 			<Header />
 			<ExploreMenu category={category} setCategory={setCategory} />
 			<FoodDisplay category={category} />
+			<AppDownload />
 		</div>
 	);
 };
