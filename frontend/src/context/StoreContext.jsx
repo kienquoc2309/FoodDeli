239f6bd3d7340ef2c0c38dda8ec50f67ffa63bcb
Tran Kien Quoc <kienquocthk9@gmail.com>
2024-12-02 00:10:41 +0700

    fetch data on fe from db

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
+import axios from 'axios';
 import { createContext, useEffect, useState } from 'react';
-import { food_list } from '../assets/assets';
 
 export const StoreContext = createContext(null);
 
@@ -7,6 +7,8 @@ const StoreContextProvider = props => {
 	const [cartItems, setCartItems] = useState({});
 	const url = 'http://localhost:5000';
 	const [token, setToken] = useState('');
+	const [food_list, setFootList] = useState([]);
+
 	const addToCart = itemId => {
 		if (!cartItems[itemId]) {
 			setCartItems(prev => ({ ...prev, [itemId]: 1 }));
@@ -29,10 +31,21 @@ const StoreContextProvider = props => {
 		return totalAmount;
 	};
 
+	const fetchFoodList = async () => {
+		try {
+			const response = await axios.get(`${url}/api/food/list`);
+			setFootList(response.data.data);
+		} catch (error) {}
+	};
+
 	useEffect(() => {
-		if (localStorage.getItem('token')) {
-			setToken(localStorage.getItem('token'));
+		async function loadData() {
+			await fetchFoodList();
+			if (localStorage.getItem('token')) {
+				setToken(localStorage.getItem('token'));
+			}
 		}
+		loadData();
 	}, []);
 
 	const contextValue = {
